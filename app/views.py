36a8b027b28a7ce253090c69528207b8f4fde883
NashaFrith <44258016+NashaFrith@users.noreply.github.com>
2024-04-26 21:16:46 -0500

    Add some routes

--- app/views.py
+++ app/views.py
@@ -22,6 +22,95 @@ def index():
 def register():
     return "Registration Page"
 
+@app.route('/register',methods=['GET','POST'])
+def register():
+    myform = SignupForm()
+
+    if myform.validate_on_submit():
+        phone_num = myform.phone_num.data
+        fullname = myform.fullname.data
+        username1 = myform.username1.data
+        password1 = myform.password1.data
+
+        flash('User registered successfully')
+        return redirect(url_for('home'))
+     
+    return render_template("contact.html", form = myform)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    myform = LoginForm()
+
+    if myform.validate_on_submit():
+        username = myform.username.data
+        password = myform.password.data
+
+        user = db.session.execute(db.select(Users).filter_by(username=username)).scalar()
+
+
+        if user is not None and check_password_hash(user.password, password):
+            remember_me = False
+
+            if 'remember_me' in request.form:
+                remember_me = True
+            login_user(user), remember = remember_me)
+
+            flash('Login Successful','success')
+            redirect(url_for('home'))
+        
+        else:
+            flash('Username or Password is incorrect.','danger')
+        
+        flash_errors(myform)
+     
+    return render_template("contact.html", form = myform)
+
+@app.route('/explore')
+
+
+@app.route('/users/{user_id}')
+
+@app.route('/posts/new')
+@login_required
+def posts():
+    # Instantiate your form class
+    form = UploadForm()
+
+    # Validate file upload on submit
+    if form.validate_on_submit():
+        # Get file data and save to your uploads folder
+        photo = form.photo.data
+        filename = secure_filename(photo.filename)
+        caption = form.caption.data
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+
+        flash('File Saved', 'success')
+        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
+
+    return render_template('upload.html',form = form)   
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('Logging Out...','Success')
+    return redirect(url_for('home'))
+
+
+@login_manager.user_loader
+def load_user(id):
+    return db.session.execute(db.select(Users).filter_by(id=id)).scalar()
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
+
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
